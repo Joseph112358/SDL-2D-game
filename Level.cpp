813342b29ebd50711a8646412aec747a323ea16e
= <=>
2024-03-28 20:47:14 +0000

    Flesh out Level class a bit

--- Level.cpp
+++ Level.cpp
@@ -1,10 +1,24 @@
 #include "Level.h"
 #include <iostream>
+#include <vector>
  // Needs to store
-    // Floor layout
+    // Floor layout (layer 0)
+    // Room layout (layer 1)
     // Wall layout
     // Objects in room
     // Aspirational: Room dimensions
+
+// Possibly pass in a map signature then reference somewhere?
+Level::Level(int width, int height, std::vector<int> floorMap,std::vector<int> itemMap){
+   width = width;
+   height = height;
+   floorMap = floorMap;
+   itemMap = itemMap;
+}
+
+int Level::getMapX(){
+   return width;
+}
  void Level::test(){
             std::cout << "test for level" << std::endl;
         }
